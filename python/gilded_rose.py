d41e07353fac5266809e75353bd2518688ed117c
Haris Fauzi <haris.fauzi@versent.com.au>
2022-03-11 15:02:46 +1000

    First try

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -11,6 +11,8 @@ class GildedRose(object):
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
+                    if item.name == "Conjured Mana Cake":
+                        item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -28,6 +30,8 @@ class GildedRose(object):
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
+                                item.quality = item.quality - 1
+                            if item.name == "Conjured Mana Cake":
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
